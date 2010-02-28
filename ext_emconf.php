67a922c0b07e6cfe2b74b74ce1bffee884175d25
rafu1987 <rafu1987@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-28 18:44:17 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commentsbe/trunk@30624 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "commentsbe".
 #
-# Auto generated 28-02-2010 19:33
+# Auto generated 28-02-2010 19:34
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"1e39";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"0f74";s:12:"ext_icon.gif";s:4:"7edf";s:14:"ext_tables.php";s:4:"55a6";s:14:"doc/manual.sxw";s:4:"1f62";s:19:"doc/wizard_form.dat";s:4:"c12a";s:20:"doc/wizard_form.html";s:4:"dfc2";s:13:"mod1/conf.php";s:4:"1b4d";s:14:"mod1/index.php";s:4:"af9d";s:18:"mod1/locallang.xml";s:4:"9ad1";s:22:"mod1/locallang_mod.xml";s:4:"e0e4";s:19:"mod1/moduleicon.gif";s:4:"ffa5";s:16:"res/bemodule.css";s:4:"7a55";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"1e39";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"0f74";s:12:"ext_icon.gif";s:4:"7edf";s:14:"ext_tables.php";s:4:"55a6";s:14:"doc/manual.sxw";s:4:"1f62";s:19:"doc/wizard_form.dat";s:4:"c12a";s:20:"doc/wizard_form.html";s:4:"dfc2";s:19:"mod1/_index_old.php";s:4:"b426";s:13:"mod1/conf.php";s:4:"1b4d";s:14:"mod1/index.php";s:4:"e84e";s:18:"mod1/locallang.xml";s:4:"9ad1";s:22:"mod1/locallang_mod.xml";s:4:"e0e4";s:19:"mod1/moduleicon.gif";s:4:"ffa5";s:16:"res/bemodule.css";s:4:"4b3d";}',
+	'suggests' => array(
+	),
 );
 
 ?>
